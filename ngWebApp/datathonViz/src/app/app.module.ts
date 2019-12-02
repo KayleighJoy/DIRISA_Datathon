2f56e397e505dae396012f57e987ad8a0f120ff0
AlexDateling <ADateling@Gmail.com>
2019-12-02 12:49:41 +0200

    Added bootstrap

--- ngWebApp/datathonViz/src/app/app.module.ts
+++ ngWebApp/datathonViz/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
