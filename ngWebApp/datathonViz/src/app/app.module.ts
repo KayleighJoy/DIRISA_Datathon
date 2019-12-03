8e35ed842aae812914e47a1b9875273b6eaf92d8
AlexDateling <ADateling@Gmail.com>
2019-12-03 19:57:33 +0200

    removed highcharts

--- ngWebApp/datathonViz/src/app/app.module.ts
+++ ngWebApp/datathonViz/src/app/app.module.ts
@@ -9,18 +9,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ChartComponent } from './chart/chart.component';
 import { ConsumePieComponent } from './consume-pie/consume-pie.component';
 import { BarRiskComponent } from './bar-risk/bar-risk.component';
-import * as Highcharts from 'highcharts';
 
 import { FormsModule } from '@angular/forms';
-import { VennChartComponent } from './venn-chart/venn-chart.component';
 @NgModule({
   declarations: [
-    
     AppComponent,
     ChartComponent,
     ConsumePieComponent,
-    BarRiskComponent,
-    VennChartComponent
+    BarRiskComponent
   ],
   imports: [
     FormsModule,
